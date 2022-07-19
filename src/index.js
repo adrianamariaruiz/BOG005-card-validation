4cea70c869cdc27bd3831cf948d04c6dbd95b0c6
Adriana Ruiz <adriannaruiz24@gmail.com>
2022-07-19 13:06:11 -0500

    validacion

--- src/index.js
+++ src/index.js
@@ -1,3 +1,68 @@
 import validator from './validator.js';
 
-console.log(validator);
+// Interaccion con el DOM
+
+const formulario = document.getElementById('formulario')
+const mesExpira = document.getElementById('mes')
+const expYear = document.getElementById('year')
+const nombreTarjeta = document.getElementById('nombreTarjeta')
+const numeroTarjeta = document.getElementById('numeroTarjeta')
+
+formulario.botonValidar.addEventListener('click', () => {
+    // console.log(validator.isValid('4083952015263'))
+
+    // pone el nombre del input en la tarjeta
+    let inputNombre = formulario.nombre.value
+    nombreTarjeta.textContent = inputNombre
+
+    // pone el mes en la tarjeta
+    let inputMes = formulario.selectMes.value
+    mesExpira.textContent = inputMes
+
+    // pone el año en la tarjeta
+    let inputYear = formulario.selectYear.value
+    expYear.textContent = inputYear
+
+    // validacion del numero de tarjeta
+    let valorInput = formulario.numTarjeta.value
+
+    if (validator.isValid(valorInput)) {
+        const mensaje = document.getElementById('mensajeExito')
+        mensaje.innerHTML = 'Su numero de tarjeta es valida'
+        mensaje.style.color = 'green'
+        mensaje.style.fontSize = '16px'
+
+        // enmascara el numero de la tarjeta en el input
+        formulario.numTarjeta.value = validator.maskify(valorInput)
+
+        // enmascara el numero de la tarjeta y lo pone en la tarjeta
+        numeroTarjeta.textContent = validator.maskify(valorInput)
+
+    } else {
+        const mensajeInvalida = document.getElementById('mensajeExito')
+        mensajeInvalida.innerHTML = 'Su numero de tarjeta es invalida'
+        mensajeInvalida.style.color = 'red'
+        mensajeInvalida.style.fontSize = '16px'
+    }
+})
+
+// Escoger el mes en el formulario
+for (let i = 1; i <= 12; i++) {
+    let opcion = document.createElement('option')
+    opcion.value = i
+    opcion.innerText = i
+    formulario.selectMes.appendChild(opcion)
+}
+
+// Escoger el año en el formulario
+const actualYear = new Date().getFullYear() /* aca obtengo el año actual */
+
+for (let i = actualYear; i <= actualYear + 8; i++) {
+    let opcion = document.createElement('option')
+    opcion.value = i
+    opcion.innerText = i
+    formulario.selectYear.appendChild(opcion)
+}
+
+
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,62 @@
+
 const validator = {
-  // ...
+    isValid: function (creditCardNumber) {
+        // console.log('valido tarjeta')
+
+        if (creditCardNumber.length === 0) {
+            return false
+        }
+        const arrayNumMultiplicados = [];
+        let contador = 0
+
+        for (let index = 0; index < creditCardNumber.length; index++) {
+            if ((index) % 2 !== 0) {
+
+                let numero = creditCardNumber[index] * 2
+
+                if (numero > 9) {
+                    numero = numero.toString()
+                    numero = Number(numero[0]) + Number(numero[1])
+                }
+
+                arrayNumMultiplicados.push(numero)
+
+                contador = contador + numero
+
+            } else {
+                arrayNumMultiplicados.push(Number(creditCardNumber[index]))
+
+                contador = contador + Number(creditCardNumber[index])
+            }
+
+        }
+
+        // console.log(arrayNumMultiplicados)
+        // console.log('contador', contador)
+
+        if (contador % 10 === 0) {
+            return true
+        } else {
+            return false
+        }
+    },
+
+    maskify: function (creditCardNumber) {
+        // console.log('funciona')
+        const numerosEnmascarar = creditCardNumber.slice(0, -4).replace(/\S/g, '#')
+        // console.log(numerosEnmascarar)
+
+        const ultimosCuatro = creditCardNumber.slice(-4)
+        // console.log(ultimosCuatro)
+
+        const cadenaNumero = numerosEnmascarar + ultimosCuatro
+
+        return cadenaNumero
+
+    }
+
 };
+
+
 
 export default validator;
